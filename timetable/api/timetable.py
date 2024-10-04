9ee42375e41dc608c4513fad3e7e94739b17eee0
chesnok <ChesnokovP1107@yandex.ru>
2024-10-04 19:59:54 +0300

    2 absolutely done first endpoint in timetable microservice!!!!

--- timetable/api/timetable.py
+++ timetable/api/timetable.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter, Depends, Security, Body, Query
 
 from core.services.timetable import TimetableService
 from core.schemas.timetable import TimetableCreate
+from core.models.timetable import TimetableModel
+from core.exceptions import AccountException
+from core.enums import Roles
 
 from timetable.depends import (
-    timetable, 
     get_accrepo,
     get_hosrepo, 
     uowdep,
@@ -19,8 +21,10 @@ timetableR = APIRouter(prefix='/Timetable', tags=['Timetable'])
 @timetableR.post('/')
 async def new_timetable(
     data: TimetableCreate, token=Security(get_token), tt=Depends(get_timetrepo)
-):
-    await introspection(token)
-    uow=uowdep(tt, get_accrepo(token), get_hosrepo(token))()
+) -> TimetableModel:
+    u = await introspection(token)
+    if not Roles.ADMIN in u.roles:
+        raise AccountException('user not admin')
+    uow = uowdep(tt, get_accrepo(token), get_hosrepo(token))()
     t = await TimetableService(uow).create(data)
     return t

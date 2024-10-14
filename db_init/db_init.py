4ae7a9f1903a04b8c2ab4dc9bee7beab852160b3
chesnok <ChesnokovP1107@yandex.ru>
2024-10-14 20:59:13 +0300

    fix add multiple accounts

--- db_init/db_init.py
+++ db_init/db_init.py
@@ -1,6 +1,8 @@
 import os
 import asyncio
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+from sqlalchemy import delete, insert, select, update
 from sqlalchemy.orm import sessionmaker
 from core.infra.postgresql.tables import Base, ACCOUNT
 
@@ -9,7 +11,7 @@ from core.enums import Roles
 DATABASE_URL = os.environ.get('POSTGRES_URL')
 
 async def main():
-    engine = create_async_engine(DATABASE_URL, echo=True)
+    engine = create_async_engine(DATABASE_URL)
 
     AsyncSessionLocal = sessionmaker(
         engine, expire_on_commit=False, class_=AsyncSession
@@ -18,14 +20,23 @@ async def main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+    async def add_if_not(session, lastName, firstName, username, password, roles):
+        stmt = select(ACCOUNT).filter_by(lastName=lastName, firstName=firstName, username=username, password=password, roles=roles)
+        res = await session.execute(stmt)
+        res = res.first()   
+        if not res:
+            user = ACCOUNT(lastName=lastName, firstName=firstName, username=username, password=password, roles=roles)
+            session.add(user)
+
     async with AsyncSessionLocal() as session:
         async with session.begin():
-            user = ACCOUNT(lastName='user', firstName='user', username='user', password='user', roles=[Roles.USER])
-            admin = ACCOUNT(lastName='admin', firstName='admin', username='admin', password='admin', roles=[Roles.ADMIN])
-            doctor = ACCOUNT(lastName='doctor', firstName='doctor', username='doctor', password='doctor', roles=[Roles.DOCTOR])
-            manager = ACCOUNT(lastName='manager', firstName='manager', username='manager', password='manager', roles=[Roles.MANAGER])
-            session.add_all([user, admin, doctor, manager])
+            await add_if_not(session, lastName='user', firstName='user', username='user', password='user', roles=[Roles.USER])
+            await add_if_not(session, lastName='admin', firstName='admin', username='admin', password='admin', roles=[Roles.ADMIN])
+            await add_if_not(session, lastName='doctor', firstName='doctor', username='doctor', password='doctor', roles=[Roles.DOCTOR])
+            await add_if_not(session, lastName='manager', firstName='manager', username='manager', password='manager', roles=[Roles.MANAGER])
+            
         await session.commit()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
